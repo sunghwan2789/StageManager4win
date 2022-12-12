ee7e4e1d3ab3291675db433cd9cb4dcb28cf5988
Sunghwan Bang <sunghwan2789@gmail.com>
2022-12-12 21:44:54 +0900

    Extract Thumbnail

--- StageManager4win/MainForm.cs
+++ StageManager4win/MainForm.cs
@@ -1,16 +1,11 @@
-using System.Diagnostics;
-using System.Numerics;
 using Windows.Win32.Foundation;
-using Windows.Win32.Graphics.Dwm;
-using static Windows.Win32.PInvoke;
 
 namespace StageManager4win;
 
 public partial class MainForm : Form
 {
-    private const int ThumbnailContentWidth = 160;
-    private const int ThumbnailContentHeight = 120;
     private const int AppMargin = 30;
+    private const int WindowMargin = 10;
 
     public MainForm()
     {
@@ -36,34 +31,16 @@ public partial class MainForm : Form
             Size tallestThumbnailSize = default;
             foreach (var window in stage.Windows.Reverse())
             {
-                var hwnd = window.Handle;
-                DwmRegisterThumbnail((HWND)Handle, hwnd, out var thumbnailId).ThrowOnFailure();
+                var thumbnail = new Thumbnail((HWND)Handle, window.Handle);
+                thumbnail.Location = windowOffset;
+                thumbnail.Update();
 
-                DwmQueryThumbnailSourceSize(thumbnailId, out var sourceSize).ThrowOnFailure();
-                var thumbnailSize = Size.Truncate((Size)sourceSize * GetFitRatio(sourceSize));
-                var thumbnailDest = new RECT(windowOffset, thumbnailSize);
-                var thumbnailProps = new DWM_THUMBNAIL_PROPERTIES
-                {
-                    dwFlags = DWM_TNP_OPACITY | DWM_TNP_SOURCECLIENTAREAONLY | DWM_TNP_VISIBLE | DWM_TNP_RECTDESTINATION,
-                    opacity = 255 * 100 / 100,
-                    fSourceClientAreaOnly = false,
-                    fVisible = true,
-                    rcDestination = thumbnailDest,
-                };
-                DwmUpdateThumbnailProperties(thumbnailId, thumbnailProps).ThrowOnFailure();
-
-                windowOffset = Point.Add(windowOffset, new Size(10, 0));
-                tallestThumbnailSize = new[] { tallestThumbnailSize, thumbnailSize }.MaxBy(x => x.Height);
+                windowOffset.Offset(WindowMargin, 0);
+                tallestThumbnailSize = new[] { tallestThumbnailSize, thumbnail.Size }.MaxBy(x => x.Height);
             }
 
-            windowOffset = new Point(0, windowOffset.Y + tallestThumbnailSize.Height + AppMargin);
+            windowOffset.X = 0;
+            windowOffset.Offset(0, tallestThumbnailSize.Height + AppMargin);
         }
-    }
-
-    private static float GetFitRatio(Size sourceSize)
-    {
-        var widthRatio = (float)ThumbnailContentWidth / sourceSize.Width;
-        var heightRatio = (float)ThumbnailContentHeight / sourceSize.Height;
-        return float.Min(widthRatio, heightRatio);
     }
 }
---
+++ StageManager4win/Thumbnail.cs
@@ -0,0 +1,67 @@
+﻿using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Dwm;
+using static Windows.Win32.PInvoke;
+
+namespace StageManager4win;
+
+public class Thumbnail : IDisposable
+{
+    public const int ThumbnailContentWidth = 160;
+    public const int ThumbnailContentHeight = 120;
+
+    private readonly nint _id;
+
+    internal Thumbnail(HWND destination, HWND source)
+    {
+        DwmRegisterThumbnail(destination, source, out _id).ThrowOnFailure();
+    }
+
+    public void Dispose()
+    {
+        DwmUnregisterThumbnail(_id);
+        GC.SuppressFinalize(this);
+    }
+
+    public Size SourceSize
+    {
+        get
+        {
+            DwmQueryThumbnailSourceSize(_id, out var size).ThrowOnFailure();
+            return size;
+        }
+    }
+
+    public Size Size
+    {
+        get
+        {
+            var sourceSize = SourceSize;
+            return Size.Truncate(sourceSize * GetFitRatio(sourceSize));
+        }
+    }
+
+    public Point Location { get; set; } = Point.Empty;
+
+    // TODO: magnify on mouse hover
+    public Rectangle Rectangle => new Rectangle(Location, Size);
+
+    public void Update()
+    {
+        var thumbnailProps = new DWM_THUMBNAIL_PROPERTIES
+        {
+            dwFlags = DWM_TNP_OPACITY | DWM_TNP_SOURCECLIENTAREAONLY | DWM_TNP_VISIBLE | DWM_TNP_RECTDESTINATION,
+            opacity = 255 * 100 / 100,
+            fSourceClientAreaOnly = false,
+            fVisible = true,
+            rcDestination = Rectangle,
+        };
+        DwmUpdateThumbnailProperties(_id, thumbnailProps).ThrowOnFailure();
+    }
+
+    private static float GetFitRatio(Size sourceSize)
+    {
+        var widthRatio = (float)ThumbnailContentWidth / sourceSize.Width;
+        var heightRatio = (float)ThumbnailContentHeight / sourceSize.Height;
+        return float.Min(widthRatio, heightRatio);
+    }
+}
